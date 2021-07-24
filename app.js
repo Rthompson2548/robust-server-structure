963c9e355bb5d2355d5b4f4408b08ed587b26a01
Reilly Thompson <80596387+Rthompson2548@users.noreply.github.com>
2021-07-23 18:00:03 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app = express()
+
+const flips = require("./data/flips-data")
+const counts = require("./data/counts-data")
+
+const countsRouter = require("./counts/counts.router")
+const flipsRouter = require("./flips/flips.router")
+
+app.use("/counts", countsRouter)
+app.use("/flips", flipsRouter)
+
+
+app.use(express.json())
+
+// Not found handler
+app.use((request, response, next) => {
+  next({ status: 404, message: `Not found: ${request.originalUrl}` })
+})
+
+// Error handler
+app.use((error, request, response, next) => {
+  console.error(error)
+  const { status = 500, message = "Something went wrong!" } = error
+  response.status(status).json({ error: message })
+})
+
+module.exports = app
